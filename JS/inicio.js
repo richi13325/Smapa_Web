ad3dd2f9afcc5e86e9906f6f0226be8a48ba6f66
richi13325 <richi13325@gmail.com>
2024-11-20 22:45:14 -0600

    last

--- JS/inicio.js
+++ JS/inicio.js
@@ -1,8 +1,16 @@
-const galeriaSection = document.querySelector('.galeria_section');
-let scrollAmount = 0;
-const scrollStep = 200; // Ajusta el desplazamiento según sea necesario
+let currentSection = 0;
+const sections = document.querySelectorAll('.galeria_section');
 
-function scrollGallery(direction) {
-    scrollAmount += direction * scrollStep;
-    galeriaSection.style.transform = `translateX(${scrollAmount}px)`;
+function navigate(direction) {
+    // Eliminar la clase 'active' de la sección actual
+    sections[currentSection].classList.remove('active');
+    
+    // Calcular la nueva sección
+    currentSection = (currentSection + direction + sections.length) % sections.length;
+
+    // Agregar la clase 'active' a la nueva sección
+    sections[currentSection].classList.add('active');
 }
+
+// Inicializa la galería mostrando la primera sección
+sections[currentSection].classList.add('active');
